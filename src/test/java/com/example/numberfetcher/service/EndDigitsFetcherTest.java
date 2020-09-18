08ed0d238716b29aefce2c22c7cb885fe5f9088c
Tobias Magnusson <tobma696@gmail.com>
2020-09-18 09:58:15 +0200

    Fix broken test

--- src/test/java/com/example/numberfetcher/service/EndDigitsFetcherTest.java
+++ src/test/java/com/example/numberfetcher/service/EndDigitsFetcherTest.java
@@ -1,12 +1,18 @@
 package com.example.numberfetcher.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.jasongoodwin.monads.Try.successful;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.jsoup.Jsoup;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class EndDigitsFetcherTest {
 
   @Mock
@@ -17,8 +23,9 @@ class EndDigitsFetcherTest {
 
   @Test
   void shouldGetEndDigits() {
-    when(webpageFetcher.get()).thenReturn(null);
-
+    final String expectedEndDigit = "14";
+    when(webpageFetcher.get()).thenReturn(successful(Jsoup.parse(String.format("<HTML><a>Slutsiffra just nu: %s</a></HTML>", expectedEndDigit))));
+    assertThat(subject.fetchEndDigits().getUnchecked(), is(expectedEndDigit));
   }
 
 }
